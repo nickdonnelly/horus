b67c49db9521be9b5e7c2086f53e3d9f94137623
Nicholas Donnelly <nick@donnelly.cc>
2018-04-03 10:26:21 +0200

    More hotkey selector tests.

--- tests/tst_settings.cpp
+++ tests/tst_settings.cpp
@@ -16,12 +16,36 @@ public:
     ~SettingsTests();
 
 private slots:
-    void hotkeys_test();
+    void initTestCase () {
+        spy_count = 0;
+        sel = new HotkeySelector("Test Label");
+        le = sel->findChild<HotkeySelectorLineEdit*>("lineEdit");
+        btnClear = sel->findChild<QPushButton*>("btnClear");
+        btnCancel = sel->findChild<QPushButton*>("btnCancel");
+        spy = new QSignalSpy(sel, SIGNAL(keyChanged(int,int)));
+    }
+
+    void cleanupTestCase() {
+        delete spy;
+        delete le;
+        delete sel;
+    }
+
+    void hotkey_selector_test();
+    void hotkey_selector_cancel();
+    void hotkey_selector_clear();
+
+private:
+    HotkeySelector * sel;
+    HotkeySelectorLineEdit* le;
+    QPushButton *btnClear, *btnCancel;
+    QSignalSpy * spy;
+
+    int spy_count;
 };
 
 SettingsTests::SettingsTests()
 {
-
 }
 
 SettingsTests::~SettingsTests()
@@ -29,18 +53,36 @@ SettingsTests::~SettingsTests()
 
 }
 
-void SettingsTests::hotkeys_test()
+void SettingsTests::hotkey_selector_test()
 {
-    HotkeySelector sel("Test Label");
-    HotkeySelectorLineEdit* le = sel.findChild<HotkeySelectorLineEdit*>("lineEdit");
-    QSignalSpy spy(&sel, SIGNAL(keyChanged(int,int)));
     QTest::mouseClick(le, Qt::LeftButton);
-    QTest::keyClick(&sel, Qt::Key_F2, Qt::ShiftModifier);
+    QTest::keyClick(sel, Qt::Key_F2, Qt::ShiftModifier);
 
-    QCOMPARE(spy.count(), 1);
+    QCOMPARE(spy->count(), ++spy_count);
 
     QKeySequence test_sequence(Qt::ShiftModifier, Qt::Key_F2);
-    QCOMPARE(test_sequence, sel.getValue());
+    QCOMPARE(test_sequence, sel->getValue());
+}
+
+void SettingsTests::hotkey_selector_cancel()
+{
+    QTest::mouseClick(le, Qt::LeftButton);
+    QTest::keyClick(sel, Qt::Key_F3);
+    QTest::mouseClick(btnClear, Qt::LeftButton);
+
+    QCOMPARE(QString(""), le->text());
+}
+
+void SettingsTests::hotkey_selector_clear()
+{
+    QTest::mouseClick(le, Qt::LeftButton);
+    QTest::keyClick(sel, Qt::Key_F2, Qt::ShiftModifier);
+
+    QCOMPARE(spy->count(), ++spy_count);
+
+    QTest::mouseClick(btnClear, Qt::LeftButton);
+
+    QCOMPARE(le->text(), QString(""));
 }
 
 DECLARE_TEST(SettingsTests)
